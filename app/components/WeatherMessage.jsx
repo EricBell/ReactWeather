823b0c2c66aee233be765da8c414ca08ed2819dd
Eric Bell <eric@polymorph.co>
2016-08-06 04:55:47 -0400

    30 stateless functional components

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,12 +1,10 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render() {
-    var {temp, location} = this.props;
-    return (
-      <h3>Its {temp} in {location}</h3>
-    );
-  }
-});
+var WeatherMessage = ({temp, location}) => {
+  
+  return (
+    <h3>Its {temp} in {location}</h3>
+  );
+}
 
 module.exports = WeatherMessage;
