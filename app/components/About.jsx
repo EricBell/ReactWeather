72f15f9750509ef3877cccc9e102157718c8c574
Eric Bell <eric@polymorph.co>
2016-08-07 13:17:34 -0400

    53 Styling about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,24 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About =  (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a sample React application coming out 
+      of the React course on Udemy.</p>
+      <div>Below are some links to resources you may find of use.</div>
+      <ul>
+        <li>
+          <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/" target="_blank">This Udemy course</a>
+        </li>
+        <li>
+          <a href="https://github.com/EricBell/ReactWeather" target="_blank">This app''s repo on Github</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org" target="_blank">Open Weather</a>
+        </li>
+      </ul>
     </div>
   );
 }
