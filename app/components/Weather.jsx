71d701d47f84003a373061ea15311c277162889c
Eric Bell <eric@polymorph.co>
2016-08-07 15:10:11 -0400

    59 add nav search support

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -14,7 +14,9 @@ var Weather  = React.createClass({
     var that = this;
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
 
     });
 
@@ -30,6 +32,22 @@ var Weather  = React.createClass({
         errorMessage: e.message
       });
     });
+  },
+  componentDidMount: function() {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash='#/';
+    }
+  },
+  componentWillReceiveProps: function(newProps) {
+    var location = newrops.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash='#/';
+    }   
   },
     render() {
       var {isLoading, temp,location, errorMessage} = this.state;
